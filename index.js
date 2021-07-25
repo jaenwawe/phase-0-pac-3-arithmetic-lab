29a29ebbdaa4e3f9b839493318e967ad514e5e86
jaenwawe <nwawe.jae@gmail.com>
2021-07-25 12:41:51 -0500

    Completed assignment

---
+++ index.js
@@ -0,0 +1,47 @@
+function add(){};
+function subtract(){};
+function multiply(){};
+function divide(){};
+function add(a,b){
+    return a + b;
+};
+
+function subtract(a, b){
+    return a - b;
+};
+
+function multiply(a,b){
+    return a *b;
+};
+
+function divide(a,b){
+    return a/b;
+};
+
+function makeInt(){
+};
+
+function increment(n)
+{
+    
+    ++n;
+    return n;
+};
+
+function decrement(n){
+    --n;
+return n;
+};
+
+function makeInt(n){
+    let num = parseInt(n, 10);
+    return num;
+}
+
+function preserveDecimal(n)
+{
+    return parseFloat(n,100);
+}
+
+function preserveDecimal(n){
+    return parseFloat(n, 100)};
